a202c11a340b1df0041507cc77c22d1a9c24f826
YahirBecerrilG <202726548+YahirBecerrilG@users.noreply.github.com>
2025-10-25 03:52:28 -0600

    cambios yahir

--- node/routes/index.js
+++ node/routes/index.js
@@ -7,6 +7,8 @@ const clienteRouter = require('./cliente');
 const accountRouter = require('./account.routes')
 const transferRouter = require('./transfer.routes');
 const openRouter = require('./openrouter.routes');
+const speechToText = require('../services/elevenlabs.service');
+const textToSpeech = require('../services/elevenlabs.service');
 
 
 function routerAPI(app){
@@ -17,6 +19,8 @@ function routerAPI(app){
     router.use('/account', accountRouter);
     router.use('/transfer', transferRouter);
     router.use('/openrouter', openRouter);
+    router.use('/sst', speechToText);
+    router.use('/tts', textToSpeech);
 }
 
 module.exports = routerAPI;
