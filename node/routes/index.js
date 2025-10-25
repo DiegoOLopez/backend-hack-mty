9ece9bbcf5ae14bc06feaf4c8c65d46bc84556de
DiegoOLopez <92211663+DiegoOLopez@users.noreply.github.com>
2025-10-25 03:42:52 -0600

    Transferencias habilitadas

--- node/routes/index.js
+++ node/routes/index.js
@@ -5,6 +5,7 @@ const express = require('express');
 const authRouter = require('./auth.routes');
 const clienteRouter = require('./cliente');
 const accountRouter = require('./account.routes')
+const transferRouter = require('./transfer.routes');
 
 
 function routerAPI(app){
@@ -13,6 +14,7 @@ function routerAPI(app){
     router.use('/auth', authRouter);
     router.use('/cliente', clienteRouter);
     router.use('/account', accountRouter);
+    router.use('/transfer', transferRouter);
 }
 
 module.exports = routerAPI;
