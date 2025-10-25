bedc212d1a66ad311b523c21951af92e5dde9d37
DiegoOLopez <92211663+DiegoOLopez@users.noreply.github.com>
2025-10-25 00:30:29 -0600

    Se incluye la llamada de cuentas

--- node/routes/index.js
+++ node/routes/index.js
@@ -4,13 +4,15 @@ const express = require('express');
 
 const authRouter = require('./auth.routes');
 const clienteRouter = require('./cliente');
+const accountRouter = require('./account.routes')
 
 
 function routerAPI(app){
     const router = express.Router();
     app.use('/api', router);
     router.use('/auth', authRouter);
-    router.use('/cliente', clienteRouter)
+    router.use('/cliente', clienteRouter);
+    router.use('/account', accountRouter);
 }
 
 module.exports = routerAPI;
