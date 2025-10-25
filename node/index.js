99ed9d3e20340a30c4a6c5b5e79c6403363cd7f8
Jean Bonilla <snapjean790@gmail.com>
2025-10-25 03:00:53 -0600

    puerto 8081

--- node/index.js
+++ node/index.js
@@ -12,7 +12,7 @@ const app = express();
 const cors = require('cors');
 
 // CONFIGURAR CORS
-const whileList = ['http://localhost:3000', 'https://myapp.co', 'http://localhost:4200', `https://${process.env.REMOTE_HOST}`];
+const whileList = ['http://localhost:3000', 'https://myapp.co', 'http://localhost:4200', `https://${process.env.REMOTE_HOST}`, `http://localhost:8081`];
 
 // Configurar OPS DE CORS
 const options = {
