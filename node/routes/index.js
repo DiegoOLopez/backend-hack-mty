624548e2ae8648f221f52c0e94c26889f6a7de21
DiegoOLopez <92211663+DiegoOLopez@users.noreply.github.com>
2025-10-24 23:38:44 -0600

    cliente get by id

--- node/routes/index.js
+++ node/routes/index.js
@@ -3,12 +3,14 @@ const express = require('express');
 
 
 const authRouter = require('./auth.routes');
+const clienteRouter = require('./cliente');
 
 
 function routerAPI(app){
     const router = express.Router();
     app.use('/api', router);
     router.use('/auth', authRouter);
+    router.use('/cliente', clienteRouter)
 }
 
 module.exports = routerAPI;
